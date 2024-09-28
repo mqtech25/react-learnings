3e58b9c96fb821183b1eeee6509294b6d51ab40f
Muhammad Qasim <mqtech25@gmail.com>
2024-09-28 23:36:15 +0500

    learning how to create custom component in react

---
+++ second-lesson-react-pack/react-app/src/customComponent.js
@@ -0,0 +1,11 @@
+
+let xComponent=()=>{
+    return(
+        <div>
+            <h1>Test Component</h1>
+            <p>This is a test component</p>
+        </div>
+    )
+}
+
+export default xComponent;
--- second-lesson-react-pack/react-app/src/index.js
+++ second-lesson-react-pack/react-app/src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import XComponent from './customComponent'
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+let root =ReactDOM.createRoot(document.getElementById('root'))
+
 root.render(
   <React.StrictMode>
     <App />
+    <XComponent/>
   </React.StrictMode>
-);
+)
+// old code from render 
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//     <XComponent />
+//   </React.StrictMode>
+//   ,
+//   document.getElementById('root')
+// )
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
