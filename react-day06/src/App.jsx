765d2c2bfd994645da48e6307f778b10eb13d9e4
Muhammad Qasim <mqtech25@gmail.com>
2026-05-05 03:10:17 +0500

    react learning

--- react-day06/src/App.jsx
+++ react-day06/src/App.jsx
@@ -1,18 +1,15 @@
 import React from "react";
-import OutlineButton from "./components/buttons/OutlineButton";
-import Header from "./components/header/Header";
 // 1 day learn folder and react flow
 // 2 day learn component
 // 3 day learn props(properties)
 // 4 day learn mini company card project objects
 // 5 day learn css module 
+// 6 day learn tailwindCss install
+
 function App(){
-    return(
-        <>
-        <Header/>
-        <OutlineButton />
-        </>
-    )
+  return(
+    <h1 className="text-2xl bg-red-900 font-bold text-white py-3 px-2">This is app component</h1>
+  )
 }
 
 export default App;
